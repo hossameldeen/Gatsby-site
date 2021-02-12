4462ec20e51f6584c691ac556c9c3fb10b3f36e5
Hossam El-Deen <hossameldeenfci@gmail.com>
2021-02-12 04:16:30 +0200

    Rename & change the text.

---
+++ src/pages/another-page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function anotherPage() {
+    return (
+        <div>
+            Now, refresh then hit the back button. You should back to the home, but instead you get 404.
+        </div>
+    )
+}
--- src/pages/pagw.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function pagw() {
-    return (
-        <div>
-            Hello
-        </div>
-    )
-}
